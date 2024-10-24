221aa65b8d98dd7a673a4af7d18d7b225f0f69e7
estherdomfeh213 <domfehesther2020@gmail.com>
2024-10-24 08:06:56 +0000

    Modify orderf table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -34,6 +34,7 @@ CREATE TABLE Orders (
   order_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT,
   order_id DATE NOT NULL,
+  order_date DATE,
   FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
