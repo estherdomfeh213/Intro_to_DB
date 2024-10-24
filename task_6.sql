d803261c0d23cd4759e5059d0f1232e4ed5c53d9
estherdomfeh213 <domfehesther2020@gmail.com>
2024-10-24 09:14:13 +0000

    revised format

--- task_6.sql
+++ task_6.sql
@@ -16,10 +16,7 @@ try:
 
     --Insert a single row into the customers table
     my_cursor.execute("""
-        INSERT INTO customer (customer_id, customer_name, email, address) VALUES 
-        (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
-        (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-        (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+        INSERT INTO customers (customer_id, customer_name, email, address) VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
 """)
 
     -- Commit the transaction
